d1f655d9d79e965f2ced9975769c6bb185aef989
Vandush <vandush@ymail.com>
2025-03-09 15:54:57 -0400

    CH2:L4; main.py prints word count of Frankenstein

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ def get_book_text(filepath):
         contents = f.read()
     return contents
 
+def wordCount(text):
+    count = 0
+    words = text.split()
+    for i in words:
+        count += 1
+    return count
 
+#text = "Hello there world!"
+#{num_words} words found in the document
 
 def main():
     path = "books/frankenstein.txt"
-    print(get_book_text(path))
+    #print(get_book_text(path))
+    text = (get_book_text(path))
+    print(f"{wordCount(text)} words found in the document")
 main()
