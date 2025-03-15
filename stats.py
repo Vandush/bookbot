88e0edfd1edc1bc4a36df975f8ff51b2d55a02bd
Vandush <vandush@ymail.com>
2025-03-14 21:07:52 -0400

    cleanup

--- stats.py
+++ stats.py
@@ -9,15 +9,10 @@ def characterCount(text):
     words = text.split()
     characters = {}
     for word in words:
-#        word = word.lower
-#        print(word)
         for character in word:
             character = character.lower()
-            #print(character)
             if character in characters:
                 characters[character] += 1
             else:
                 characters[character] = 1
     return characters
-    #for i in words:
-        #print(i)
